fa012aafc9cb97323d77aac97edcf692703b2678
Vitor Costa <vmcosta@me.com>
2024-03-23 22:34:03 +0000

    Ok

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -39,10 +39,7 @@ class ChatGPTTelegramBot:
         self.openai = openai
         bot_language = self.config['bot_language']
         self.commands = [
-            BotCommand(command='help', description=localized_text('help_description', bot_language)),
-            BotCommand(command='reset', description=localized_text('reset_description', bot_language)),
-            BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
-            BotCommand(command='resend', description=localized_text('resend_description', bot_language))
+
         ]
         # If imaging is enabled, add the "image" command to the list
         if self.config.get('enable_image_generation', False):
@@ -69,11 +66,7 @@ class ChatGPTTelegramBot:
         bot_language = self.config['bot_language']
         help_text = (
                 localized_text('help_text', bot_language)[0] +
-                '\n\n' +
-                '\n'.join(commands_description) +
-                '\n\n' +
                 localized_text('help_text', bot_language)[1] +
-                '\n\n' +
                 localized_text('help_text', bot_language)[2]
         )
         await update.message.reply_text(help_text, disable_web_page_preview=True)
